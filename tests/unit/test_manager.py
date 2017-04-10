a39c89115a8bc4555a4351ea4307b5db2f5f5d4a
kjwon15 <kjwonmail@gmail.com>
2017-04-10 12:57:53 +0900

    Add unit test for regex group kwargs

--- tests/unit/test_manager.py
+++ tests/unit/test_manager.py
@@ -15,3 +15,17 @@ def test_import_plugin_single_module():
     assert 'fake_plugin_module' not in sys.modules
     PluginsManager()._load_plugins('fake_plugin_module')
     assert 'fake_plugin_module' in sys.modules
+
+
+def test_kwargs():
+    pm = PluginsManager()
+    pm._load_plugins('kwarg_plugin_module')
+
+    msg = {
+        'category': 'default_reply',
+        'text': 'Hello, Jeong Arm',
+    }
+
+    func, args, kwargs = pm.get_plugins(msg['category'], msg['text'])
+    assert args == ()
+    assert kwargs == {'name': 'Jeong Arm'}
