0d6e18a75a80c8e5cf638652f1270526c45b4c61
TetraEtc <administrator@tetraetc.com>
2017-04-22 05:24:28 +1000

    Fix bot breaking when new users join groups/the team (#141)
    * Fix bot breaking when new users join groups/the team
    
    * Moved to event_type variable over repeated event.gets

--- slackbot/dispatcher.py
+++ slackbot/dispatcher.py
@@ -138,9 +138,15 @@ class MessageDispatcher(object):
         while True:
             events = self._client.rtm_read()
             for event in events:
-                if event.get('type') != 'message':
-                    continue
-                self._on_new_message(event)
+                event_type = event.get('type')
+                if event_type == 'message':
+                    self._on_new_message(event)
+                elif event_type in ['channel_created', 'group_joined', 'im_created']:
+                    channel = [event['channel']]
+                    self._client.parse_channel_data(channel)
+                elif event_type == 'team_join':
+                    user = event['user']
+                    self._client.users[user['id']] = user
             time.sleep(1)
 
     def _default_reply(self, msg):
