8e4807cdedf8d466278bb15c2430f82cf00e387a
kotarou777775 <kriw@localhost.localdomain>
2017-05-22 20:15:26 +0900

    add header for download file

--- slackbot/utils.py
+++ slackbot/utils.py
@@ -11,9 +11,9 @@ import six
 logger = logging.getLogger(__name__)
 
 
-def download_file(url, fpath):
+def download_file(url, fpath, token=''):
     logger.debug('starting to fetch %s', url)
-    r = requests.get(url, stream=True)
+    r = requests.get(url, stream=True, headers={"Authorization": "Bearer "+token})
     with open(fpath, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024*64):
             if chunk:  # filter out keep-alive new chunks
