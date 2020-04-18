7a24eff9c3e23cf37fba6dffa94d61845558a09a
massongit <suzukimasaya428@gmail.com>
2020-04-18 21:28:53 +0900

    Revert "Changed to compile regular expression"
    This reverts commit 1861b2d05b367c2cfd061a7483c39db3ac4945ab.

--- slackbot/manager.py
+++ slackbot/manager.py
@@ -16,8 +16,6 @@ class PluginsManager(object):
     def __init__(self):
         pass
 
-    _space_pattern = re.compile(r'\s')
-
     commands = {
         'respond_to': {},
         'listen_to': {},
@@ -70,7 +68,7 @@ class PluginsManager(object):
         if text is None:
             text = ''
         text = text.strip()
-        text = self._space_pattern.sub(' ', text)
+        text = re.sub(r'\s', ' ', text)
         for matcher in self.commands[category]:
             m = matcher.search(text)
             if m:
