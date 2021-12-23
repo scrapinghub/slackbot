0afff163eac55522e9bb5623172212a6ccf62b45
Jacob Truman <jatruman@adobe.com>
2021-12-23 09:30:22 -0700

    Update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ __version__ = open(join(dirname(__file__), 'slackbot/VERSION')).read().strip()
 install_requires = (
     'requests>=2.4.0',
     'websocket-client>=0.22.0,<=0.44.0',
-    'slacker>=0.9.50',
+    'slack-sdk>=3.13.0',
     'six>=1.10.0'
 ) # yapf: disable
 
